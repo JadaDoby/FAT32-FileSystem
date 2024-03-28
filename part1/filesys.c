6efaedb8ca4adeb37e1ac97d9cab1e3701bcab43
shell155 <sb22bg@fsu.edu>
2024-03-28 14:37:08 -0400

    update version

--- part1/filesys.c
+++ part1/filesys.c
@@ -9,6 +9,8 @@ typedef struct {
     uint32_t totalSectors;
     uint32_t FATSize; // Sectors per FAT
     uint32_t rootCluster;
+    uint16_t reservedSectors; // Added: Number of reserved sectors
+    uint8_t numFATs; // Added: Number of FATs
 } FAT32BootSector;
 
 FAT32BootSector bs;
@@ -20,30 +22,42 @@ int mountImage(const char *imageName) {
         return -1;
     }
 
-    // Example offsets may need adjustment based on your FAT32 structure
-    fseek(fp, 11, SEEK_SET); fread(&bs.bytesPerSector, sizeof(bs.bytesPerSector), 1, fp);
-    fseek(fp, 13, SEEK_SET); fread(&bs.sectorsPerCluster, sizeof(bs.sectorsPerCluster), 1, fp);
-    fseek(fp, 32, SEEK_SET); fread(&bs.totalSectors, sizeof(bs.totalSectors), 1, fp);
-    fseek(fp, 36, SEEK_SET); fread(&bs.FATSize, sizeof(bs.FATSize), 1, fp);
-    fseek(fp, 44, SEEK_SET); fread(&bs.rootCluster, sizeof(bs.rootCluster), 1, fp);
+    // Read bytes per sector
+    fseek(fp, 11, SEEK_SET);
+    fread(&bs.bytesPerSector, sizeof(bs.bytesPerSector), 1, fp);
+    // Read sectors per cluster
+    fread(&bs.sectorsPerCluster, sizeof(bs.sectorsPerCluster), 1, fp);
+    // Read reserved sectors
+    fread(&bs.reservedSectors, sizeof(bs.reservedSectors), 1, fp);
+    // Read number of FATs
+    fread(&bs.numFATs, sizeof(bs.numFATs), 1, fp);
+
+    // Seek and read total sectors
+    fseek(fp, 32, SEEK_SET);
+    fread(&bs.totalSectors, sizeof(bs.totalSectors), 1, fp);
+    // Read sectors per FAT
+    fread(&bs.FATSize, sizeof(bs.FATSize), 1, fp);
+    // Read root cluster
+    fread(&bs.rootCluster, sizeof(bs.rootCluster), 1, fp);
 
     fclose(fp);
     return 0;
 }
 
 void printInfo() {
+    uint32_t totalDataSectors = bs.totalSectors - (bs.reservedSectors + (bs.FATSize * bs.numFATs * bs.sectorsPerCluster));
+    uint64_t totalClusters = totalDataSectors / bs.sectorsPerCluster;
+    
     printf("Bytes Per Sector: %d\n", bs.bytesPerSector);
     printf("Sectors Per Cluster: %d\n", bs.sectorsPerCluster);
     printf("Root Cluster: %d\n", bs.rootCluster);
-    printf("Total # of Clusters in Data Region: %llu\n", (uint64_t)bs.totalSectors / bs.sectorsPerCluster);
-    printf("# of Entries in One FAT: %d\n", bs.FATSize * (bs.bytesPerSector / 4)); // 4 bytes per FAT entry assumption
+    printf("Total # of Clusters in Data Region: %llu\n", totalClusters);
+    printf("# of Entries in One FAT: %d\n", bs.FATSize * (bs.bytesPerSector / 4)); // Assuming 4 bytes per FAT entry
     printf("Size of Image (in bytes): %llu\n", (uint64_t)bs.totalSectors * bs.bytesPerSector);
 }
 
 void cleanup() {
-    // In this example, there's no dynamic memory to free, but if you allocate memory, free it here.
-    // e.g., if you had dynamically allocated memory, you would free it like so:
-    // free(your_allocated_resource);
+    // Placeholder for cleanup operations
 }
 
 void startShell(const char* imageName) {
